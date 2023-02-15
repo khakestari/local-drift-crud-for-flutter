c4b48b4120dcca03245f86e5ccaa771357240f1f
Mostafa Nematollah zade <mostafanmz@yahoo.com>
2023-02-16 02:50:28 +0330

    Add date picker to create form

--- lib/Screens/Create/widget/form.dart
+++ lib/Screens/Create/widget/form.dart
@@ -1,7 +1,9 @@
 import 'package:crudsample/Screens/Create/widget/inputfield.dart';
+import 'package:crudsample/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class form extends StatelessWidget {
+class form extends StatefulWidget {
   const form({
     Key? key,
   }) : super(key: key);
@@ -12,7 +14,13 @@ class form extends StatelessWidget {
   static final PhoneNumberController = TextEditingController();
   static final EmailController = TextEditingController();
   static final BankAccountNumberController = TextEditingController();
+  static DateTime? _dateOfBirth;
 
+  @override
+  State<form> createState() => _formState();
+}
+
+class _formState extends State<form> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -21,28 +29,37 @@ class form extends StatelessWidget {
         children: <Widget>[
           inputfield(
             lable: 'Firstname',
-            controller: FirstnameController,
+            controller: form.FirstnameController,
           ),
           const SizedBox(
             height: 20,
           ),
           inputfield(
             lable: 'Lastname',
-            controller: LastnameController,
+            controller: form.LastnameController,
           ),
           const SizedBox(
             height: 20,
           ),
-          inputfield(
-            lable: 'DateOfBirth',
-            controller: DateOfBirthController,
+          TextField(
+            style: TextStyle(
+              color: Colors.white,
+            ),
+            decoration: InputDecoration(
+                labelText: 'DateOfBirth', labelStyle: TextStyle(color: color2)),
+            controller: form.DateOfBirthController,
+            onTap: () => pickDateOfBith(context),
           ),
+          // inputfield(
+          //   lable: 'DateOfBirth',
+          //   controller: form.DateOfBirthController,
+          // ),
           const SizedBox(
             height: 20,
           ),
           inputfield(
             lable: 'PhoneNumber',
-            controller: PhoneNumberController,
+            controller: form.PhoneNumberController,
             type: TextInputType.number,
           ),
           const SizedBox(
@@ -50,18 +67,45 @@ class form extends StatelessWidget {
           ),
           inputfield(
             lable: 'Email',
-            controller: EmailController,
+            controller: form.EmailController,
           ),
           const SizedBox(
             height: 20,
           ),
           inputfield(
             lable: 'BankAccountNumber',
-            controller: BankAccountNumberController,
+            controller: form.BankAccountNumberController,
             type: TextInputType.number,
           )
         ],
       ),
     );
   }
+  Future<void> pickDateOfBith(BuildContext context) async {
+    final initialDate = DateTime.now();
+    final newDate = await showDatePicker(
+        context: context,
+        initialDate: form._dateOfBirth ?? initialDate,
+        firstDate: DateTime(DateTime.now().year - 100),
+        lastDate: DateTime(DateTime.now().year + 1),
+        builder: (context, child) => Theme(
+            data: ThemeData().copyWith(
+              colorScheme: const ColorScheme.light(
+                  primary: Color.fromARGB(255, 111, 196, 213),
+                  onPrimary: Colors.black,
+                  onSurface: Colors.white),
+              dialogBackgroundColor: color1,
+              shadowColor: color2,
+            ),
+            child: child ?? const Text('')));
+    if (newDate == null) {
+      return;
+    }
+    setState(() {
+      form._dateOfBirth = newDate;
+      String dob = DateFormat('MM/dd/yyyy').format(newDate);
+      form.DateOfBirthController.text = dob;
+    });
+  }
 }
+
